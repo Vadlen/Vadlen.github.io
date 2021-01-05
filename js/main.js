15b80a80bab5e6ab28c21172c16ca77de6cc1c87
Vadlen <76511289+Vadlen@users.noreply.github.com>
2021-01-05 17:25:07 +0500

    js

---
+++ js/main.js
@@ -0,0 +1,221 @@
+
+
+
+
+document.getElementById('cat_1').addEventListener('mouseover', () => {
+    document.getElementById('1').src = '/img/icon/course/1_1.png'
+})
+document.getElementById('cat_1').addEventListener('mouseout', () => {
+    document.getElementById('1').src = '/img/icon/course/1.png'
+})
+
+document.getElementById('cat_2').addEventListener('mouseover', () => {
+    document.getElementById('2').src = '/img/icon/course/2_1.png'
+})
+document.getElementById('cat_2').addEventListener('mouseout', () => {
+    document.getElementById('2').src = '/img/icon/course/2.png'
+})
+
+document.getElementById('cat_3').addEventListener('mouseover', () => {
+    document.getElementById('3').src = '/img/icon/course/3_1.png'
+})
+document.getElementById('cat_3').addEventListener('mouseout', () => {
+    document.getElementById('3').src = '/img/icon/course/3.png'
+})
+
+document.getElementById('cat_4').addEventListener('mouseover', () => {
+    document.getElementById('4').src = '/img/icon/course/4_1.png'
+})
+document.getElementById('cat_4').addEventListener('mouseout', () => {
+    document.getElementById('4').src = '/img/icon/course/4.png'
+})
+
+document.getElementById('cat_5').addEventListener('mouseover', () => {
+    document.getElementById('5').src = '/img/icon/course/5_1.png'
+})
+document.getElementById('cat_5').addEventListener('mouseout', () => {
+    document.getElementById('5').src = '/img/icon/course/5.png'
+})
+
+document.getElementById('cat_6').addEventListener('mouseover', () => {
+    document.getElementById('6').src = '/img/icon/course/6_1.png'
+})
+document.getElementById('cat_6').addEventListener('mouseout', () => {
+    document.getElementById('6').src = '/img/icon/course/6.png'
+})
+
+document.getElementById('cat_7').addEventListener('mouseover', () => {
+    document.getElementById('7').src = '/img/icon/course/7_1.png'
+})
+document.getElementById('cat_7').addEventListener('mouseout', () => {
+    document.getElementById('7').src = '/img/icon/course/7.png'
+})
+
+document.getElementById('cat_8').addEventListener('mouseover', () => {
+    document.getElementById('8').src = '/img/icon/course/8_1.png'
+})
+document.getElementById('cat_8').addEventListener('mouseout', () => {
+    document.getElementById('8').src = '/img/icon/course/8.png'
+})
+
+document.getElementById('cat_9').addEventListener('mouseover', () => {
+    document.getElementById('9').src = '/img/icon/course/9_1.png'
+})
+document.getElementById('cat_9').addEventListener('mouseout', () => {
+    document.getElementById('9').src = '/img/icon/course/9.png'
+})
+
+document.getElementById('cat_10').addEventListener('mouseover', () => {
+    document.getElementById('10').src = '/img/icon/course/10_1.png'
+})
+document.getElementById('cat_10').addEventListener('mouseout', () => {
+    document.getElementById('10').src = '/img/icon/course/10.png'
+})
+
+document.getElementById('cat_11').addEventListener('mouseover', () => {
+    document.getElementById('11').src = '/img/icon/course/11_1.png'
+})
+document.getElementById('cat_11').addEventListener('mouseout', () => {
+    document.getElementById('11').src = '/img/icon/course/11.png'
+})
+
+document.getElementById('cat_12').addEventListener('mouseover', () => {
+    document.getElementById('12').src = '/img/icon/course/12_1.png'
+})
+document.getElementById('cat_12').addEventListener('mouseout', () => {
+    document.getElementById('12').src = '/img/icon/course/12.png'
+})
+
+
+// courses
+
+document.getElementById('crs_1').addEventListener('mouseover', () => {
+    document.getElementById('crs_11').src = '/img/courses/7.png'
+    document.querySelector('img').style.opacity= '0.50%';
+    document.querySelector('img').style.filter= 'alpha(opacity=40)';
+} )
+document.getElementById('crs_1').addEventListener('mouseout', () => {
+    document.getElementById('crs_11').src = '/img/courses/1.png'
+    
+})
+
+document.getElementById('crs_2').addEventListener('mouseover', () => {
+    document.getElementById('crs_22').src = '/img/courses/7.png'
+} )
+document.getElementById('crs_2').addEventListener('mouseout', () => {
+    document.getElementById('crs_22').src = '/img/courses/2.jpg'
+})
+
+document.getElementById('crs_3').addEventListener('mouseover', () => {
+    document.getElementById('crs_33').src = '/img/courses/7.png'
+} )
+document.getElementById('crs_3').addEventListener('mouseout', () => {
+    document.getElementById('crs_33').src = '/img/courses/3.png'
+})
+
+document.getElementById('crs_4').addEventListener('mouseover', () => {
+    document.getElementById('crs_44').src = '/img/courses/7.png'
+} )
+document.getElementById('crs_4').addEventListener('mouseout', () => {
+    document.getElementById('crs_44').src = '/img/courses/4.png'
+})
+
+document.getElementById('crs_5').addEventListener('mouseover', () => {
+    document.getElementById('crs_55').src = '/img/courses/7.png'
+} )
+document.getElementById('crs_5').addEventListener('mouseout', () => {
+    document.getElementById('crs_55').src = '/img/courses/5.png'
+})
+
+document.getElementById('crs_6').addEventListener('mouseover', () => {
+    document.getElementById('crs_66').src = '/img/courses/7.png'
+} )
+document.getElementById('crs_6').addEventListener('mouseout', () => {
+    document.getElementById('crs_66').src = '/img/courses/6.png'
+})
+
+
+// best
+
+document.getElementById('bes_1').addEventListener('mouseover', () => {
+    document.getElementById('bes_11').src = '/img/courses/7.png'
+} )
+document.getElementById('bes_1').addEventListener('mouseout', () => {
+    document.getElementById('bes_11').src = '/img/courses/9.png'
+})
+
+document.getElementById('bes_2').addEventListener('mouseover', () => {
+    document.getElementById('bes_22').src = '/img/courses/7.png'
+} )
+document.getElementById('bes_2').addEventListener('mouseout', () => {
+    document.getElementById('bes_22').src = '/img/courses/10.png'
+})
+
+document.getElementById('bes_3').addEventListener('mouseover', () => {
+    document.getElementById('bes_33').src = '/img/courses/7.png'
+} )
+document.getElementById('bes_3').addEventListener('mouseout', () => {
+    document.getElementById('bes_33').src = '/img/courses/11.png'
+})
+
+document.getElementById('bes_4').addEventListener('mouseover', () => {
+    document.getElementById('bes_44').src = '/img/courses/7.png'
+} )
+document.getElementById('bes_4').addEventListener('mouseout', () => {
+    document.getElementById('bes_44').src = '/img/courses/12.png'
+})
+
+document.getElementById('bes_5').addEventListener('mouseover', () => {
+    document.getElementById('bes_55').src = '/img/courses/7.png'
+} )
+document.getElementById('bes_5').addEventListener('mouseout', () => {
+    document.getElementById('bes_55').src = '/img/courses/13.png'
+})
+
+document.getElementById('bes_6').addEventListener('mouseover', () => {
+    document.getElementById('bes_66').src = '/img/courses/7.png'
+} )
+document.getElementById('bes_6').addEventListener('mouseout', () => {
+    document.getElementById('bes_66').src = '/img/courses/14.png'
+})
+
+//
+
+document.getElementById('google').addEventListener('mouseover', () => {
+    document.getElementById('google').src = '/img/client_logo/google_color.png'
+} )
+document.getElementById('google').addEventListener('mouseout', () => {
+    document.getElementById('google').src = '/img/client_logo/google.png'
+})
+document.getElementById('google_1').addEventListener('mouseover', () => {
+    document.getElementById('google_1').src = '/img/client_logo/google_color.png'
+} )
+document.getElementById('google_1').addEventListener('mouseout', () => {
+    document.getElementById('google_1').src = '/img/client_logo/google.png'
+})
+
+document.getElementById('envato').addEventListener('mouseover', () => {
+    document.getElementById('envato').src = '/img/client_logo/envato_color.png'
+} )
+document.getElementById('envato').addEventListener('mouseout', () => {
+    document.getElementById('envato').src = '/img/client_logo/envato.png'
+})
+document.getElementById('envato_1').addEventListener('mouseover', () => {
+    document.getElementById('envato_1').src = '/img/client_logo/envato_color.png'
+} )
+document.getElementById('envato_1').addEventListener('mouseout', () => {
+    document.getElementById('envato_1').src = '/img/client_logo/envato.png'
+})
+
+
+document.getElementById('heart_1').addEventListener('click', () => {
+    document.getElementById('heart_1').src = '/img/blog/heart-red.png'
+} )
+
+document.getElementById('heart_2').addEventListener('click', () => {
+    document.getElementById('heart_2').src = '/img/blog/heart-red.png'
+} )
+
+document.getElementById('heart_3').addEventListener('click', () => {
+    document.getElementById('heart_3').src = '/img/blog/heart-red.png'
+} )
