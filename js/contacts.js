1b4c5517bd67765bbfb443cd3fb6db03e8dab1e2
Vadlen <76511289+Vadlen@users.noreply.github.com>
2021-01-27 13:20:08 +0500

    js

---
+++ js/contacts.js
@@ -0,0 +1,6 @@
+document.getElementById('cont').addEventListener('mouseover', () => {
+    document.getElementById('cont1').src = '/img/tellR.png'
+})
+document.getElementById('cont').addEventListener('mouseout', () => {
+    document.getElementById('cont1').src = '/img/tell.png'
+})
---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+document.getElementById('block1').addEventListener('mouseover', () => {
+    document.getElementById('star1').src = '/img/kotalog/star.png';
+})
+document.getElementById('block1').addEventListener('mouseout', () => {
+    document.getElementById('star1').src = '/img/kotalog/star1.png'
+})
+
+document.getElementById('block2').addEventListener('mouseover', () => {
+    document.getElementById('star2').src = '/img/kotalog/star.png';
+})
+document.getElementById('block2').addEventListener('mouseout', () => {
+    document.getElementById('star2').src = '/img/kotalog/star1.png'
+})
+
+document.getElementById('block3').addEventListener('mouseover', () => {
+    document.getElementById('star3').src = '/img/kotalog/star.png'
+})
+document.getElementById('block3').addEventListener('mouseout', () => {
+    document.getElementById('star3').src = '/img/kotalog/star1.png'
+})
+
+document.getElementById('block4').addEventListener('mouseover', () => {
+    document.getElementById('star4').src = '/img/kotalog/star.png'
+})
+document.getElementById('block4').addEventListener('mouseout', () => {
+    document.getElementById('star4').src = '/img/kotalog/star1.png'
+})
+
+document.getElementById('block5').addEventListener('mouseover', () => {
+    document.getElementById('star5').src = '/img/kotalog/star.png'
+})
+document.getElementById('block5').addEventListener('mouseout', () => {
+    document.getElementById('star5').src = '/img/kotalog/star1.png'
+})
+
+document.getElementById('block6').addEventListener('mouseover', () => {
+    document.getElementById('star6').src = '/img/kotalog/star.png'
+})
+document.getElementById('block6').addEventListener('mouseout', () => {
+    document.getElementById('star6').src = '/img/kotalog/star1.png'
+})
+
+
+
+
+// contacts
+
+
